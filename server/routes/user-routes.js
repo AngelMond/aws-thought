271bdd65f24b55888084eff42f1a6c6fbb7b5686
AngelMondragon <angel.mondragon@4n.com.mx>
2023-03-16 00:44:17 -0600

    created get method

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 
-
 const AWS = require('aws-sdk');
 const awsConfig = {
   region: 'us-east-2',
@@ -12,3 +11,20 @@ const awsConfig = {
 AWS.config.update(awsConfig);
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 const table = 'Thoughts';
+
+
+  router.get('/users', (req, res) => {
+    const params = {
+      TableName: table,
+    };
+    // Scan return all items in the table
+    dynamodb.scan(params, (err, data) => {
+      if (err) {
+        res.status(500).json(err); // an error occurred
+      } else {
+        res.json(data.Items);
+      }
+    });
+  });
+
+  module.exports = router;
